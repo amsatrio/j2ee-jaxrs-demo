78006d43ab6596f747383766e5d3ae19dbd37ab4
Andre Mochammad Satrio <andre.satrio@ms.mii.co.id>
2025-07-30 13:34:35 +0700

    add logger middleware

---
+++ src/main/java/io/github/amsatrio/middleware/LoggerMiddleware.java
@@ -0,0 +1,29 @@
+package io.github.amsatrio.middleware;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Provider
+public class LoggerMiddleware implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        log.info("filter request");
+        log.info("url: " + requestContext.getUriInfo().getRequestUri().toString());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        log.info("filter response");
+        log.info("status: " + responseContext.getStatus());
+        log.info("body: " + responseContext.getEntity());
+    }
+}
